d782a45062ad8ad3e7c7f9f6652ba652b2bb8529
mdeboer1 <mdeboer1@L11108042787.wctc.msft>
2014-08-25 18:59:36 -0500

    deleted top comment and added code to main.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package firstgit;
 
 /**
@@ -17,7 +11,7 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         
-        
+        System.out.println("Hello world");
         
     }
     
